caa4e7046fe5cca3940f98928e5a366215b7de1d
sun <1010244243@qq.com>
2021-07-21 16:33:02 +0800

    first commit

--- src/main/java/com/sun/gittest.java
+++ src/main/java/com/sun/gittest.java
@@ -1,8 +1,13 @@
 package com.sun;
 
+import java.sql.SQLOutput;
+
 /**
  * @author sun
  * @date 2021/7/21 - 16:11
  */
 public class gittest {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
 }
